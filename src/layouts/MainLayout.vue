8366e3383b8a97713e90ebab8c9a088c5c8b5a1b
PatricioPedro <patriciowilderbarrospedro@gmail.com>
2021-10-12 01:45:42 +0100

    Buttom to go to the login page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,17 +2,27 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-       <q-btn
-        v-if="$route.fullPath==='/chat'"
-         icon="arrow_back"
-         dense
-         label="Back"
-         flat
-         to="/"
-       />
+        <q-btn
+          v-if="$route.fullPath === '/chat'"
+          icon="arrow_back"
+          dense
+          label="Back"
+          flat
+          to="/"
+        />
         <q-toolbar-title class="absolute-center">
           {{ title }}
         </q-toolbar-title>
+        <q-btn
+          v-if="$route.fullPath === '/chat'"
+          class="absolute-right q-mr-sm"
+          icon="account_circle"
+          dense
+          label="Login"
+          no-caps
+          flat
+          to="/auth"
+        />
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -22,35 +32,31 @@
 </template>
 
 <script>
-
-
-import { defineComponent, computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { defineComponent, computed } from "vue";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-  },
- setup () {
-   /**
-    *  This computed property will return dynamically the layout's title corresponding its route page 
-    */
+  components: {},
+  setup() {
+    /**
+     *  This computed property will return dynamically the layout's title corresponding its route page
+     */
     const title = computed(() => {
-      const $route = useRoute() 
-      if ($route.fullPath === '/') {
-        return 'SmackChat'
-      } else if($route.fullPath === '/auth') {
-        return 'Login'
-      } else if ($route.fullPath === '/chat') {
-        return 'Chat'
+      const $route = useRoute();
+      if ($route.fullPath === "/") {
+        return "SmackChat";
+      } else if ($route.fullPath === "/auth") {
+        return "Login";
+      } else if ($route.fullPath === "/chat") {
+        return "Chat";
       }
-
-    })
+    });
 
     return {
-      title
-    }
-  }
-})
+      title,
+    };
+  },
+});
 </script>
